f2c05f5db2b26c5f539a3f4b7996ecd2b6807b4d
Martin Abdulghni <martin.abdulghni@navcite.se>
2023-11-17 05:48:56 +0100

    ok

--- script/projects.js
+++ script/projects.js
@@ -5,6 +5,16 @@ setTimeout(function () {
   hero.addClass("hero-black");
 }, 1500);
 
+var maxBody = $(".max-body");
+var unloaded = $(".unloaded");
+
+setTimeout(function () {
+  maxBody.removeClass("max-body");
+}, 3100);
+setTimeout(function () {
+  unloaded.removeClass("unloaded");
+}, 3100);
+
 var mySlider = {
   config: {
     slider: ".slider-content",
@@ -257,7 +267,7 @@ gsap.from(".slider-content", {
   right: "-140%",
   ease: "expo.inOut",
 });
-gsap.from(".card", {
+gsap.from(".card-holder", {
   duration: 1.5,
   stagger: 0.2,
   delay: 4,
@@ -271,3 +281,38 @@ gsap.from("#banner", {
   bottom: "-100%",
   ease: "expo.inOut",
 });
+
+var sliderCounter = 0;
+var sliderContent = ["MaRtIn","Abdulghni"];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = "";
+
+  sliderValue.classList.remove("holder-animation");
+  void sliderValue.offsetWidth;
+  sliderValue.classList.add("holder-animation");
+
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == " ") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv);
+  }
+
+  sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
